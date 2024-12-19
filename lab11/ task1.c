a3151f6b951cd65846722a46111ef862a2269d4e
PavelVOLK1929 <pavelvolevatch@yandex.ru>
2024-12-19 12:33:32 +0700

    added lab11

---
+++ lab11/ task1.c
@@ -0,0 +1,22 @@
+//  размер массива  чаров некорректный
+
+#include <stdio.h>  
+  
+typedef struct  
+{  
+char str[6];      
+int num;  
+} NumberRepr;  
+  
+void format(NumberRepr* number)  
+{      
+sprintf(number->str, "%3d", number->num);  
+}   
+int main()  
+{  
+NumberRepr number = { .num = 1025 };  
+format(&number);  
+printf("str: %s\n", number.str);      
+printf("num: %d\n", number.num);  
+return 0;  
+}  
---
+++ lab11/task2.c
@@ -0,0 +1,11 @@
+// СКОБКИИ!!!
+
+#include <stdio.h>  
+  
+#define SQR(x) (x * x)  
+int main() {      
+int y = 5;      
+int z = SQR(y + 1);      
+printf("z = %d\n", z);      
+return 0;
+}  
