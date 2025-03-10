53b70251335f61e9365f6e8b0810f2a3800fb8ca
PavelVOLK1929 <pavelvolevatch@yandex.ru>
2025-03-10 22:07:43 +0700

    first_commit

---
+++ C_programming_2_semestr!/lab2_git/sort.c
@@ -0,0 +1,30 @@
+void ShakerSort(int * A, int n, int * M, int * C){
+    int L = 0;
+    int R = n-1;
+    int k = n-1;
+
+    (*M) = 0;
+    (*C) = 0;
+    do{
+        for(int j = R ; j > L ; j--){
+            (*C)++;if (A[j]<A[j-1]){
+                (*M)++;int tmp = A[j];
+                (*M)++;A[j] = A[j-1];
+                (*M)++;A[j-1] = tmp;
+                k = j;
+            }
+        }
+        L = k;
+        for(int j = L ; j < R; j++){
+            (*C)++;if (A[j]>A[j+1]){
+                (*M)++;int tmp = A[j];
+                (*M)++;A[j] = A[j+1];
+                (*M)++;A[j+1] = tmp;
+                k = j;
+            }
+        }
+        R = k;
+
+    }while(L<R);
+
+}
